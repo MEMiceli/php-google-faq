884e71721a0f956407635661fb35b3ad0c68ea36
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-23 16:19:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+$faqs = [
+        [
+            'question' => 'Post 1',
+            'answer' => 'Michele Papagni',
+        ],
+        [
+            'question' => 'Post 1',
+            'answer' => 'Michele Papagni',
+        ],
+        [
+            'question' => 'Post 1',
+            'answer' => 'Michele Papagni',
+        ],
+        [
+            'question' => 'Post 1',
+            'answer' => 'Michele Papagni',
+        ],
+        [
+            'question' => 'Post 1',
+            'answer' => 'Michele Papagni',
+        ],
+        [
+            'question' => 'Post 1',
+            'answer' => 'Michele Papagni',
+        ],
+];
+
+// foreach ($posts as $date => $articles) {
+//     echo $date;
+//     }
+//     foreach ($articles as $article)
+//         echo $article['title'];
+//         echo $article['author'];
+//         echo $article['text'];
+//     {
+    
+// }
+?>
