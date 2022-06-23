95f614117a424dbfcc3e5b782e3b213189d738ed
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-23 17:09:29 +0200

    container senza dimensioni

--- index.php
+++ index.php
@@ -49,3 +49,21 @@ $faqs = [
     
 // }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <?php foreach($faqs as $faq){
+            echo $faq['question'];
+            echo $faq['answer'];
+        }
+        ?>
+    </div>
+</body>
+</html>
